b3d88d19f33c49e0e67e79f39e364ba09e7250f7
Israel <israel.palomar@doubledot.es>
2018-08-07 18:34:52 +0200

    Listado de algunos de los datos en la ficha

--- src/components/views/symbol-view.vue
+++ src/components/views/symbol-view.vue
@@ -1,7 +1,40 @@
 <template lang="html">
 
   <section class="symbol-view">
-    <h1>symbol-view Component</h1>
+
+
+    <template v-if="!!data">
+
+      <h1 v-if="!!data.name" v-html="data.name"></h1>
+      <h2 v-if="!!data.currency" v-html="data.currency.name"></h2>
+      <h3 v-if="!!data.issuer" v-html="data.issuer.name"></h3>
+
+      <p v-if="!!data.isin" v-html="data.isin"></p>
+
+   <!--    <p class="region" v-if="!!data.region">
+        
+        <span v-if="!!data.region.name" v-html="data.region.name"></span>
+        <span v-if="!!data.region.region_level2" v-html="data.region.region_level2.name"></span>
+        <span v-if="!!data.region.region_level3" v-html="data.region.name"></span>
+
+      </p> -->
+
+      <p class="risk-family" v-if="data.risk_family">
+          <a v-html="data.risk_family.name"></a>
+          <a v-html="data.risk_family.sub_family.name"></a>
+      </p>
+
+      <!-- TODO SECTOR -->
+
+
+      <!-- TODO GRAFICO -->
+
+
+
+    </template>
+
+
+
   </section>
 
 </template>
