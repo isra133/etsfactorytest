b414a64bc89504efa1cd2474d2867e73614b59c3
Israel <israel.palomar@doubledot.es>
2018-08-08 18:43:04 +0200

    visualizacion de la grafica y de parte de la ficha

---
+++ src/components/parts/chart-visualizer.vue
@@ -0,0 +1,48 @@
+<template lang="html">
+
+  <section class="chart-visualizer">
+ <line-chart :data="data"></line-chart>
+  
+</section>
+
+</template>
+
+<script lang="js">
+
+import Vue from 'vue'
+import VueChartkick from 'vue-chartkick'
+import Chart from 'chart.js'
+
+Vue.use(VueChartkick, {adapter: Chart})
+
+
+  export default  {
+    name: 'chart-visualizer',
+    props: ['datasets'],
+    mounted() {
+
+      this.datasets.forEach(d => {
+        let data = [ d.date , d.value];
+        this.data.push(data);
+      });
+
+    },  
+    data() {
+      return {
+         data : []
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+.chart-visualizer {
+
+}
+</style>
--- src/components/views/symbol-view.vue
+++ src/components/views/symbol-view.vue
@@ -1,15 +1,46 @@
 <template lang="html">
 
-  <section class="symbol-view">
+  <section class="wrapper -symbol-view">
 
 
     <template v-if="!!data">
 
-      <h1 v-if="!!data.name" v-html="data.name"></h1>
-      <h2 v-if="!!data.currency" v-html="data.currency.name"></h2>
-      <h3 v-if="!!data.issuer" v-html="data.issuer.name"></h3>
+      <div class="container -big">
 
-      <p v-if="!!data.isin" v-html="data.isin"></p>
+        <div class="data-container" v-if="!!data.name">
+          <span class="assistant">Nombre</span>
+          <h1 v-html="data.name"></h1>
+        </div>
+
+
+        <div class="data-container" v-if="!!data.currency">
+          <span class="assistant">Divisa</span>
+          <h2 v-html="data.currency.name"></h2>
+        </div>
+
+
+        <div class="data-container" v-if="!!data.issuer">
+          <span class="assistant">Emisor</span>
+          <h2 v-html="data.issuer.name"></h2>
+        </div>
+
+        <div class="data-container" v-if="!!data.isin">
+          <span class="assistant">ISIN</span>
+          <h2 v-html="data.isin"></h2>
+        </div>
+
+
+        <div class="data-container">
+          <span class="assistant">Familia de riesgo</span>
+          <p class="breadcrumb row-start" v-if="data.risk_family">
+            <h2 v-html="data.risk_family.name"></h2>
+            <h2 v-html="data.risk_family.sub_family.name"></h2>
+          </p>
+        </div>
+        
+
+      </div>
+
 
    <!--    <p class="region" v-if="!!data.region">
         
@@ -19,25 +50,23 @@
 
       </p> -->
 
-      <p class="risk-family" v-if="data.risk_family">
-        <a v-html="data.risk_family.name"></a>
-        <a v-html="data.risk_family.sub_family.name"></a>
-      </p>
+
 
       <!-- TODO SECTOR -->
 
 
       <!-- TODO GRAFICO -->
+      <chart :datasets="data.prices" v-if="!!data"></chart>
 
     </template>
 
-    <comments-form :id="47868"></comments-form>
+    <!-- <comments-form :id="47868"></comments-form>
     <comments-list :id="47868"></comments-list>
+  -->
 
 
 
-
-  </section>
+</section>
 
 </template>
 
@@ -47,6 +76,7 @@
 
   import commentsForm from '@/components/parts/comments-form';
   import commentsList from '@/components/parts/comments-list';
+  import chart from '@/components/parts/chart-visualizer';
 
   export default  {
     name: 'symbol-view',
@@ -68,13 +98,51 @@
     },
     components : {
       'comments-form' : commentsForm,
-      'comments-list' : commentsList
+      'comments-list' : commentsList,
+      'chart' : chart
     }
   }
 </script>
 
 <style scoped lang="scss">
-.symbol-view {
-
+.assistant{
+  width: 100%;
+  text-align: left;
+  display: inline-block;
+  font-size: 12px;
+  color: #333;
 }
+
+.data-container{
+  margin-bottom: 40px;
+}
+
+.data-container h1{
+  text-align: left;
+  width: 100%;
+  display: inline-block;
+  font-weight: 700;
+  color: #333;
+  font-size: 28px;
+}
+
+.data-container h2{
+  text-align: left;
+  width: 100%;
+  display: inline-block;
+  font-weight: 700;
+  color: #333;
+  font-size: 24px;
+}
+
+.breadcrumb h2{
+  width: auto;
+}
+
+.breadcrumb h2::before{
+  content: '/';
+  display: inline-block;
+  
+}
+
 </style>
